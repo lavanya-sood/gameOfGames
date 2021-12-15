0eeae1a6ea432175db5cefd01cb2592dc2bfb64f
Lavanya Sood <lavanyasood09@gmail.com>
2021-12-15 22:47:17 +1100

    Finished Tic Tac Toe

--- src/games/tictactoe/TicTacToe.js
+++ src/games/tictactoe/TicTacToe.js
@@ -6,12 +6,8 @@ import Button from '@mui/material/Button'
 const TicTacToe = () => {
 
     const [board,setBoard] = React.useState([null,null,null,null,null,null,null,null,null]);
-    // let board = [null,null,null,null,null,null,null,null,null];
     let [player1,setPlayer] = React.useState(true);
-    //let player1 = true;
-    // const [win,setWin] = React.useState(false);
     const [tictac,setTicTac] = React.useState(null);
-   // const win = checkWin
 
     const winStates = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
     
@@ -46,46 +42,16 @@ const TicTacToe = () => {
     }
 
     let gameStatus = checkStatus(board,win);
-    // React.useEffect(() => {
-    //     updateBoard();
-    // },[]);
 
-    //const updateBoard = () => {
-        //setTicTac(board.map((item,id) => <div class="playerPos" onClick={checkPress} id={id}> {item}</div>));
-    //};
-
-   
-
-    // const toggle = React.useCallback(() => {
-    //     console.log("In here PLAYER = ",player1);
-    //     setPlayer1(player1 => !player1);
-    //     console.log("In here PLAYER2 = ",player1);
-    //   }, []);
 
     const resetBoard = () => {
         setBoard([null,null,null,null,null,null,null,null,null]);
         gameStatus = null;
+        setPlayer(true);
     }
       
     const checkPress = (e) => {
         const pos = e.target.id;
-
-
-
-        // if (board[pos] == null) {
-            
-        //     board[pos] =  player1 ? 'X' : 'O';
-
-        //     //toggle();
-        //     //player1 = !player1
-        //     setPlayer1(!player1);
-        //     //console.log(player1);
-
-        //     //updateBoard();
-        //     checkWin();
-            
-            
-        // } 
 
         let tempBoard = [...board];
 
@@ -107,11 +73,10 @@ const TicTacToe = () => {
                     {board.map((item,id) => <div class="playerPos" onClick={checkPress} id={id}> {item}</div>)}
                 </div>
                 <div id="TicTacPlayer">
-                    <h2> Turn </h2>
-                    <p> <span>X</span> Player 1</p>
-                    <p> <span>O</span> Player 2</p>
-                    <p>{gameStatus}</p>
+                    <p> <span>X</span> {player1 ? <u>Player 1 </u> : <span>Player 1</span>}</p>
+                    <p> <span>O</span> {player1 ? <span>Player 2 </span> : <u>Player 2</u>}</p>
                     <Button variant="contained" onClick={resetBoard}>Reset</Button>
+                    <p class="winStatus">{gameStatus}</p>
                 </div>
             </div>
             
